891ba079bc1a834dee6924e505f890d51f1e62de
Joel Grant <joelmgrant@me.com>
2025-07-14 14:17:44 -0600

    feat: set up landing page

--- spec/features/homepage/index_spec.rb
+++ spec/features/homepage/index_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe "Homepage", type: :feature do
   end
 
   it "displays the app's goal" do
-    expect(page).to have_content("Easily track the little details of your Disney trips, and save them forever!")
+    expect(page).to have_content("Save even the finest details about your rides, meals and stops during your Disney Vacation!")
   end
+
+  it "has a link to log in" do
+    expect(page).to have_link("Log in")
+  end
+  
+  it "has a link to get started" do
+    expect(page).to have_link("Get started")
+  end
+
 end
