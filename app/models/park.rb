6aa3160ac888fac4efc37abb286bf448d491bcab
Joel Grant <joelmgrant@me.com>
2025-07-15 14:34:50 -0600

    update parks

--- app/models/park.rb
+++ app/models/park.rb
@@ -2,10 +2,10 @@ class Park < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :location, presence: true
 
-  # has_many :attractions
-  # has_many :restaurants
-  # has_many :shows
-  # has_many :characters
-  # has_many :events
-  # has_many :modes_of_transportation
+  has_many :attractions
+  has_many :restaurants
+  has_many :shows
+  has_many :characters
+  has_many :events
+  has_many :modes_of_transportation
 end
