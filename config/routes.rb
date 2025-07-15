5289283bcee7bc3acb5a36b1ea54204c71a52036
Joel Grant <joelmgrant@me.com>
2025-07-15 09:30:22 -0600

    feat: add user login flow

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,8 @@ Rails.application.routes.draw do
   resources :users, only: [ :create ]
 
   resources :dashboard, only: [ :index ]
+
+  get "/login", to: "sessions#new", as: :login
+  post "/login", to: "sessions#create"
+  # delete "/logout", to: "sessions#destroy", as: :logout
 end
